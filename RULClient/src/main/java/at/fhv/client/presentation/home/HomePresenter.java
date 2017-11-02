575665979bedb86bd1c405fa64f1b75c14344415
cbl6499 <clemens.blecha@students.fhv.at>
2017-11-02 12:01:31 +0100

    home controller

--- RULClient/src/main/java/at/fhv/client/presentation/home/HomePresenter.java
+++ RULClient/src/main/java/at/fhv/client/presentation/home/HomePresenter.java
@@ -1,4 +1,17 @@
 package at.fhv.client.presentation.home;
 
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+
 public class HomePresenter {
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+
 }
