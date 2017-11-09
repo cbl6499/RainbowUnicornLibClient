e3f9f51839a5d2ff0237964838d71de1460938d4
dhu92 <dhutter92@gmail.com>
2017-11-09 19:50:10 +0100

    changes RMIMediaSearch

--- RULClient/src/main/java/at/fhv/team3/rmi/interfaces/RMIMediaSearch.java
+++ RULClient/src/main/java/at/fhv/team3/rmi/interfaces/RMIMediaSearch.java
@@ -1,14 +1,19 @@
 package at.fhv.team3.rmi.interfaces;
 
+
 import at.fhv.team3.domain.dto.DTO;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ClemensB on 06.11.17.
  */
 public interface RMIMediaSearch extends Remote{
     public ArrayList<ArrayList<DTO>> search(String searchTerm) throws RemoteException;
+    public List<DTO> getAllBookDTOs() throws RemoteException;
+    public List<DTO> getAllDvdDTOs() throws RemoteException;
+    public List<DTO> getAllMagazineDTOs() throws RemoteException;
 }
