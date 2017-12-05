ef6a19b353dcdcab330b0d5f847ec640082b0831
dhu92 <dhutter92@gmail.com>
2017-12-05 18:55:26 +0100

    changes serverip plsfix

--- RULClient/src/main/java/at/fhv/team3/App.java
+++ RULClient/src/main/java/at/fhv/team3/App.java
@@ -1,10 +1,20 @@
 package at.fhv.team3;
 
+import at.fhv.team3.applicationbean.interfaces.RemoteSearchBeanFace;
+import at.fhv.team3.domain.dto.BookDTO;
+import at.fhv.team3.domain.dto.DTO;
 import at.fhv.team3.presentation.home.HomeView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.awt.print.Book;
+import java.util.List;
+import java.util.Properties;
 
 public class App extends Application {
 
@@ -23,11 +33,39 @@ public class App extends Application {
         stage.setScene(scene);
         stage.getIcons().add(new Image("logo.png"));
         stage.show();
+
+      //  connect();
     }
 
     @Override
     public void stop() throws Exception {
 
+    }
+
+    private void connect() {
+        try {
+            //TODO: import glassfish
+            Properties props = new Properties();
+            props.setProperty("java.naming.factory.initial",
+                    "com.sun.enterprise.naming.SerialInitContextFactory");
+            props.setProperty("java.naming.factory.url.pkgs",
+                    "com.sun.enterprise.naming");
+            props.setProperty("java.naming.factory.state",
+                    "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700"); //3700 Glassfish default port
+
+            InitialContext ctx = new InitialContext(props);
+
+            RemoteSearchBeanFace remoteInterface = (RemoteSearchBeanFace) ctx.lookup("java:global/RainbowUnicornLib/at.fhv.team3.applicationbean.interfaces.RemoteSearchBeanFace");
+            System.out.println(remoteInterface.getBooksByISBN(""));
+            List<DTO> dto = remoteInterface.getAllBookDTOs();
+            for(DTO d : dto){
+                System.out.println(d.getId());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
---
+++ RULClient/src/main/java/at/fhv/team3/applicationbean/interfaces/RemoteSearchBeanFace.java
@@ -0,0 +1,33 @@
+package at.fhv.team3.applicationbean.interfaces;
+
+import at.fhv.team3.domain.dto.BookDTO;
+import at.fhv.team3.domain.dto.DTO;
+import at.fhv.team3.domain.dto.DvdDTO;
+import at.fhv.team3.domain.dto.MagazineDTO;
+import at.fhv.team3.domain.interfaces.Borrowable;
+import at.fhv.team3.domain.interfaces.Searchable;
+
+import javax.ejb.Remote;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Remote
+public interface RemoteSearchBeanFace extends Serializable {
+
+    public List<DTO> getAllBookDTOs();
+
+    public List<DTO> getAllDvdDTOs();
+
+    public List<DTO> getAllMagazineDTOs();
+
+    public ArrayList<ArrayList<Searchable>> searchMedias(String searchTerm);
+
+    public ArrayList<ArrayList<DTO>> search(String searchTerm);
+
+    public ArrayList<BookDTO> getBooksByISBN(String isbn);
+
+    public ArrayList<DvdDTO> getDvdByTitle(String title);
+
+    public ArrayList<MagazineDTO> getMagazinesByTitleAndEdition(String title, String edition);
+}
